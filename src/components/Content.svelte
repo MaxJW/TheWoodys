6be9760efcc3da38d7fedffabd0b5dabdf2f02f8
Max Wilson <maxjw98@gmail.com>
2022-11-15 10:33:27 +0000

    Fixed form content

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -2,7 +2,15 @@
 </script>
 
 <div class="content">
-    <!-- Insert Form Content Here -->
+    <iframe
+        title="The Woodys Form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeyn7ZxSpbLs4-eDjLKtx-Ph4P5NK8-XB6BPo-oxf0U0f-9Ew/viewform?embedded=true"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0">Loading…</iframe
+    >
 </div>
 
 <style>
@@ -10,6 +18,11 @@
         position: relative;
         z-index: 1;
         margin-top: 4em;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 10vh;
     }
     :global(.ff-powered-img) {
         visibility: hidden;
