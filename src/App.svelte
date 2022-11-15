5574c3e801a7d6743fd3d99e7fabc6f803c67f56
Max Wilson <maxjw98@gmail.com>
2022-11-15 12:21:54 +0000

    New build, new items!

--- src/App.svelte
+++ src/App.svelte
@@ -18,79 +18,7 @@
 
 <Blur />
 
-<p style="text-align: center; color: #beba9f;">Sponsored by Young Sheldon</p>
-<div class="desc">
-    <div class="image">
-        <img src="images/presenter.png" alt="Presenter" />
-    </div>
-    <div class="text">
-        <h1>REMEMBERING THE BEST OF 2022</h1>
-        <p>
-            Ending 2022 with a bang by remembering the greatest, funniest, sad boi, and definitely
-            hottest moments of the year. Have your say by nominating and voting to commemorate your
-            favourite moments of this year.
-        </p>
-    </div>
-</div>
-
 <Content />
 
 <!-- Keep at bottom of page -->
 <CustomParticles />
-
-<style>
-    .desc {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: row;
-    }
-    img {
-        max-width: 100%;
-        width: 800px;
-        filter: drop-shadow(0px 5px 20px #000);
-    }
-    .image {
-        display: flex;
-        padding-right: 29px;
-        justify-content: center;
-    }
-    .text {
-        font-size: 20px;
-        padding-left: 20px;
-    }
-
-    .text h1 {
-        font-size: 3em;
-        color: rgb(190, 186, 159);
-    }
-
-    .text p {
-        font-size: 1.25em;
-        color: rgb(190, 186, 159);
-    }
-
-    @media only screen and (max-width: 700px) {
-        .desc {
-            flex-direction: column;
-        }
-
-        img {
-            max-height: 100%;
-            height: 400px;
-            width: auto;
-        }
-
-        .text {
-            text-align: center;
-        }
-
-        .text h1 {
-            font-size: 2em;
-        }
-
-        .text p {
-            font-size: 1em;
-        }
-    }
-</style>
--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
+    function scrollIntoView({ target }) {
+        const el = document.querySelector(target.getAttribute('href'));
+        if (!el) return;
+        el.scrollIntoView({
+            behavior: 'smooth',
+        });
+    }
 </script>
 
-<div class="content">
+<p class="sponsor">Sponsored by Young Sheldon</p>
+<div class="desc">
+    <div class="image">
+        <img src="images/presenter.png" alt="Presenter" />
+    </div>
+    <div class="text">
+        <h1>REMEMBERING THE BEST OF 2022</h1>
+        <p>
+            Ending 2022 with a bang by remembering the greatest, funniest, sad boi, and definitely
+            hottest moments of the year. Have your say by nominating and voting to commemorate your
+            favourite moments of this year.
+        </p>
+        <input
+            type="button"
+            value="Vote Here"
+            href="#form"
+            on:click|preventDefault={scrollIntoView}
+        />
+    </div>
+</div>
+
+<div class="content" id="form">
     <iframe
         title="The Woodys Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLSeyn7ZxSpbLs4-eDjLKtx-Ph4P5NK8-XB6BPo-oxf0U0f-9Ew/viewform?embedded=true"
@@ -14,10 +42,74 @@
 </div>
 
 <style>
+    .sponsor {
+        text-align: center;
+        color: #beba9f;
+    }
+
+    .desc {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: row;
+    }
+
+    img {
+        max-width: 100%;
+        width: 800px;
+        filter: drop-shadow(0px 5px 20px #000);
+    }
+
+    .image {
+        display: flex;
+        padding-right: 29px;
+        justify-content: center;
+    }
+
+    .text {
+        font-size: 20px;
+        padding-left: 20px;
+    }
+
+    .text h1 {
+        font-size: 3em;
+        color: rgb(190, 186, 159);
+    }
+
+    .text p {
+        font-size: 1.25em;
+        color: rgb(190, 186, 159);
+    }
+
+    @media only screen and (max-width: 700px) {
+        .desc {
+            flex-direction: column;
+        }
+
+        img {
+            max-height: 100%;
+            height: 400px;
+            width: auto;
+        }
+
+        .text {
+            text-align: center;
+        }
+
+        .text h1 {
+            font-size: 2em;
+        }
+
+        .text p {
+            font-size: 1em;
+        }
+    }
+
     .content {
         position: relative;
         z-index: 1;
         margin-top: 4em;
+        scroll-margin: 5vh;
     }
 
     .content iframe {
